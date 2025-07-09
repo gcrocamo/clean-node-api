b7b4dda441403ad39ad1d8650ce0b134c44c4e5a
gcrocamo <guilhermecrocamo@exitlag.com>
2025-07-08 22:30:42 -0300

    test: update SignupController test to check for missing name

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,9 +1,10 @@
+import { SignUpController } from './signup'
+
 describe('Signup Controller', () => {
   test('should return 400 if no name is provided', () => {
     const sut = new SignUpController()
     const httpRequest = {
       body: {
-        name: 'any_name',
         email: 'any_email@mail.com',
         password: 'any_password',
         passwordConfirmation: 'any_password'
