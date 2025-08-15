63676b59166f94322871727ef1a28db1839d5f81
gcrocamo <guilhermecrocamo@exitlag.com>
2025-08-14 22:51:15 -0300

    test: improve formatting and consistency in signup tests

--- src/presentation/controllers/signup.spec.ts
+++ src/presentation/controllers/signup.spec.ts
@@ -1,17 +1,31 @@
-import { SignUpController } from './signup'
+import { SignUpController } from "./signup";
 
-describe('Signup Controller', () => {
-  test('should return 400 if no name is provided', () => {
-    const sut = new SignUpController()
+describe("Signup Controller", () => {
+  test("should return 400 if no name is provided", () => {
+    const sut = new SignUpController();
     const httpRequest = {
       body: {
-        email: 'any_email@mail.com',
-        password: 'any_password',
-        passwordConfirmation: 'any_password'
-      }
-    }
-    const httpResponse = sut.handle(httpRequest)
-    expect(httpResponse.statusCode).toBe(400)
-    expect(httpResponse.body).toEqual(new Error('Missing param: name'))
-  })
-})
+        email: "any_email@mail.com",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: name"));
+  });
+
+  test("should return 400 if no email is provided", () => {
+    const sut = new SignUpController();
+    const httpRequest = {
+      body: {
+        name: "any_name",
+        password: "any_password",
+        passwordConfirmation: "any_password",
+      },
+    };
+    const httpResponse = sut.handle(httpRequest);
+    expect(httpResponse.statusCode).toBe(400);
+    expect(httpResponse.body).toEqual(new Error("Missing param: email"));
+  });
+});
