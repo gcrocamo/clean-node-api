94f0781d50b2cbf5af412e03f20c6a1e3df8cc88
gcrocamo <crocamoguilherme@gmail.com>
2025-09-27 14:03:00 -0300

    fix: correct formatting and structure in jest.config.ts and tsconfig.json

--- jest.config.ts
+++ jest.config.ts
@@ -1,13 +1,13 @@
-import type {Config} from 'jest';
+import type { Config } from 'jest'
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir/src/**/*.ts'],
-  coverageDirectory: "coverage",
+  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
     '.+\\.ts$': 'ts-jest'
-  },
-};
+  }
+}
 
-export default config;
+export default config
