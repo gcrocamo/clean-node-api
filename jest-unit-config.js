efe3325dbabb51e07365dfde3ddb8c8bc6f7a362
gcrocamo <crocamoguilherme@gmail.com>
2025-10-06 11:04:11 -0300

    chore: add jest configs and scripts

---
+++ jest-unit-config.js
@@ -0,0 +1,3 @@
+import config from './jest.config.js'
+config.testMatch = ['**/*.spec.ts']
+export default config
--- jest.config.ts
+++ jest.config.ts
@@ -2,7 +2,11 @@ import type { Config } from 'jest'
 
 const config: Config = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
+  collectCoverageFrom: [
+    '<rootDir>/src/**/*.ts',
+    '!<rootDir>/src/**/*protocols*.ts',
+    '!<rootDir>/src/**/protocols/**'
+  ],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   transform: {
