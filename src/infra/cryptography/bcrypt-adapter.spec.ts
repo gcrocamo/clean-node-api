7ba917a079919943d30fc7fe947f63165121142f
gcrocamo <crocamoguilherme@gmail.com>
2025-10-26 18:17:46 -0300

    test: update BcryptAdapter tests to use async/await for consistency

--- src/infra/cryptography/bcrypt-adapter.spec.ts
+++ src/infra/cryptography/bcrypt-adapter.spec.ts
@@ -1,12 +1,24 @@
 import bcrypt from 'bcrypt'
 import { BcrypAdapter } from './bcrypt-adapter'
 
+jest.mock('bcrypt', () => ({
+  async hash (): Promise<string> {
+    return new Promise(resolve => resolve('hash'))
+  }
+}))
+
 describe('Bcrypt Adapter', () => {
-  test('should call bcrypt with correct value', () => {
+  test('should call bcrypt with correct value', async () => {
     const salt = 12
     const sut = new BcrypAdapter(salt)
     const hashSpy = jest.spyOn(bcrypt, 'hash')
-    sut.encrypt('any_value')
+    await sut.encrypt('any_value')
     expect(hashSpy).toHaveBeenCalledWith('any_value', salt)
+  })
+  test('should return a hash on success', async () => {
+    const salt = 12
+    const sut = new BcrypAdapter(salt)
+    const hash = await sut.encrypt('any_value')
+    expect(hash).toBe('hash')
   })
 })
