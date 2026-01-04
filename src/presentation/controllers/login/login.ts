6cb57d813537e786dc2e54f9af282ec0f2a151b0
gcrocamo <crocamoguilherme@gmail.com>
2026-01-04 16:55:51 -0300

    feat: ensure LoginController returns 200 if valid credentials are provided

--- src/presentation/controllers/login/login.ts
+++ src/presentation/controllers/login/login.ts
@@ -1,5 +1,5 @@
 import { InvalidParamError, MissingParamError } from '../../errors'
-import { badRequest, serverError, unauthorized } from '../../helpers/http-helpers'
+import { badRequest, serverError, success, unauthorized } from '../../helpers/http-helpers'
 import type { Controller, HttpRequest, HttpResponse, EmailValidator, Authentication } from './login-protocols'
 
 export class LoginController implements Controller {
@@ -28,7 +28,7 @@ export class LoginController implements Controller {
       if (!accessToken) {
         return unauthorized()
       }
-      return { statusCode: 200, body: {} }
+      return success({ accessToken })
     } catch (error) {
       return serverError(error as Error)
     }
